6062f6bcefc19e544784ef2e0f61725d37750169
Zezuz <mczanaty92@gmail.com>
2021-03-16 20:33:23 -0500

    DID IT!

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //properties
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public double Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            if ("RRMmmmrrrr" == EngineNoise)
+            { 
+                Console.WriteLine("This Car can go!.");
+            }
+            else if ("VROOM" == EngineNoise)
+                        {
+                Console.WriteLine("This car FlYS.");
+            }
+
+            else if ("Brap" == EngineNoise)
+            {
+                Console.WriteLine("This Car Makes some Noise.");
+            }
+            else
+            {
+
+            }
+        }
+
+        public void MakeHonkNoise()
+        {
+            if ("Beep" == HonkNoise)
+            {
+                Console.WriteLine("It Beeps for sure.");
+            }
+            else if ("Boop" == HonkNoise)
+            {
+                Console.WriteLine("For some reason this one Boops, Kinda weird.");
+            }
+            else if ("Beep Bop" == HonkNoise)
+            {
+                Console.WriteLine("I'm not really sure but it definitely drives.");
+
+            }
+            else 
+            {
+
+            }
+        }
+
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,6 +13,43 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            //Base Constructor
+            Car bmw = new Car();
+            bmw.Year = 2016;
+            bmw.Make = "BMW";
+            bmw.Model = "325i";
+            bmw.EngineNoise = "RRMmmmrrrr";
+            bmw.HonkNoise = "Beep";
+            bmw.IsDriveable = true;
+
+            //Object Initializer Syntax
+            Car Octane = new Car { Year = 2015, Make = "Octane", Model = "Octane Rush", EngineNoise = "VROOM", HonkNoise = "Boop", IsDriveable = true };
+            
+            //
+            Car Mustang = new Car();
+            Mustang.Year = 2019;
+            Mustang.Make = "Mustang";
+            Mustang.Model = "Mach 1";
+            Mustang.EngineNoise = "Brap";
+            Mustang.HonkNoise = "Beep Bop";
+            Mustang.IsDriveable = true;
+            
+
+            Console.WriteLine($"This car is a {Mustang.Year} {Mustang.Make}  and it's a {Mustang.Model}." );
+            Mustang.MakeEngineNoise();
+            Mustang.MakeHonkNoise();
+            Console.WriteLine("");
+
+            Console.WriteLine($"This car is a {Octane.Year} {Octane.Make} and it's a {Octane.Model}.");
+            Octane.MakeEngineNoise();
+            Octane.MakeHonkNoise();
+            Console.WriteLine("");
+
+            Console.WriteLine($"This car is a {bmw.Year} {bmw.Make} and it is a {bmw.Model}.");
+            bmw.MakeEngineNoise();
+            bmw.MakeHonkNoise();
+            Console.WriteLine("");
+
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
